bf693aaa495c46b366c56657a174d04117850587
MohsenDehghankar <mohsendehghankar@gmail.com>
2026-05-06 15:50:01 -0700

    assigned expeirments

--- benchmark_area/experiments/accuracy/run_all.sh
+++ benchmark_area/experiments/accuracy/run_all.sh
@@ -102,7 +102,7 @@ run_math() {
 # run_ruler louver_ta_budget_s512 --method louver_ta   --threshold_mode budget --budget_fraction 0.1 --sample_size 512 --n_samples 50
 # [SERVER1-DONE]
 # run_ruler h2o_f0.1           --method h2o         --budget_fraction 0.1                  --n_samples 50
-# [SERVER1-RUNNING]
+# [SERVER1-DONE]
 # run_ruler quest_f0.1         --method quest        --budget_fraction 0.1                 --n_samples 50
 # [SERVER1-RUNNING]
 # run_ruler streaming_llm_f0.1 --method streaming_llm --budget_fraction 0.1               --n_samples 50
@@ -114,11 +114,14 @@ run_aime louver_ta_budget_f15      --method louver_ta  --threshold_mode budget -
 # run_aime dense_sdpa                --method dense_sdpa --max_problems 20
 # [SERVER2-RUNNING]
 # run_aime twilight_p0.85            --method twilight   --top_p 0.85 --max_problems 20
-# [SERVER1]
+# [-]
 # run_aime h2o_b512                  --method h2o        --budget_tokens 512 --max_problems 20
 
+# [HERE]
 # run_math louver_ta_budget_f15      --method louver_ta  --threshold_mode budget --budget_fraction 0.15 --sample_size 512 --max_samples 50
+# [SERVER2]
 # run_math dense_sdpa                --method dense_sdpa               --max_samples 50
+# [-]
 # run_math twilight_p0.85            --method twilight   --top_p 0.85 --max_samples 50
 
 echo "ALL DONE" | tee -a logs/summary.log
